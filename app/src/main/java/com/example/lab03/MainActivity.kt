24a72c152d74b33f833b326c9cd4a172b08e9be1
Exxasens <oleg-okatev@hotmail.com>
2024-02-13 00:56:29 +0300

    Change ids of elements. Add TaskModel to MainActivity

--- app/src/main/java/com/example/lab03/MainActivity.kt
+++ app/src/main/java/com/example/lab03/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.lab03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+
+public class TaskModel(val id: Int, val name: String, val description: String) {
+}
 
 class MainActivity : AppCompatActivity() {
+    val notes = arrayOf(
+        TaskModel(0, "First", "Desc"),
+        TaskModel(1, "Second", "Desc"),
+        TaskModel(2, "Third", "Desc")
+    )
+
+    val noteName = findViewById<EditText>(R.id.editNote)
+    val noteDescription = findViewById<EditText>(R.id.editNoteDescription)
+    val addNoteButton = findViewById<Button>(R.id.addNoteButton)
+    val saveNoteButton = findViewById<Button>(R.id.saveNoteButton)
+    val showNoteButton = findViewById<Button>(R.id.showNoteButton)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
